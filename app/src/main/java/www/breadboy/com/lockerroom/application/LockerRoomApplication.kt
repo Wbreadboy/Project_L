330587b8e91ae8c2939dce27a807e906ae4aaa10
Wbreadboy <dadyboys@naver.com>
2017-09-07 12:11:52 +0900

    [add] Start Realm & initalized

--- app/src/main/java/www/breadboy/com/lockerroom/application/LockerRoomApplication.kt
+++ app/src/main/java/www/breadboy/com/lockerroom/application/LockerRoomApplication.kt
@@ -3,6 +3,7 @@ package www.breadboy.com.lockerroom.application
 import android.app.Activity
 import android.app.Application
 import android.content.Context
+import io.realm.Realm
 import www.breadboy.com.lockerroom.base.ComponentBuilder
 import www.breadboy.com.lockerroom.base.WhichSubcomponentBuilders
 import javax.inject.Inject
@@ -21,11 +22,12 @@ class LockerRoomApplication : Application(), WhichSubcomponentBuilders {
         super.onCreate()
 
         setupLockerRoomModule()
+        setupRealm()
     }
 
-    private fun setupLockerRoomModule() {
-        DaggerLockerRoomComponent.create().inject(this)
-    }
+    private fun setupLockerRoomModule() = DaggerLockerRoomComponent.create().inject(this)
+
+    private fun setupRealm() = Realm.init(this)
 
     override fun getComponentBuilder(activity: Class<out Activity>) = lockerRoomComponentBuilders[activity]!!.get()
 
